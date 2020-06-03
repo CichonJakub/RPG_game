db153ecad197ec7b96debe8cfaa8b5baa0cc4bdf
mciborowski33 <mciborowski33@gmail.com>
2020-06-03 17:21:20 +0200

    basic server

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 //server.js
+const http = require('http');
+const fs = require('fs');
+const socket = require('socket.io');
+const express = require('express');
+const app = express();
+
+const port = 80;
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
